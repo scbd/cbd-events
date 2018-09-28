79204a92bad84fa27421d6f68da90d4072f60d1b
Randy Houlahan <randy.houlahan@cbd.int>
2018-09-28 09:32:13 -0400

    hack for language bug, conf home screen article

--- middleware/redirects.js
+++ middleware/redirects.js
@@ -1,4 +1,17 @@
-export default function ({ route, redirect }) {
+export default async function ({ router, route, redirect, store }) {
+  //get confrences if we do not have
+  if(!store.state.conferences.selected)
+    await store.dispatch('conferences/get')
 
-  console.log('middleware route',route)
+  // this is a hack to fix bug from nuxt-i18n
+  // the bug is load in default switch locale then switch back default routes are lost warning
+  // reactive values on side menu do not change when locale changes
+  // this work around fixes
+  if(!store.state.conferences.selected || !store.state.conferences.selectedMeeting) return
+  let conferenceCode = store.state.conferences.selected.code
+  let meetingCode = store.state.conferences.selectedMeeting.code
+  if(route.name==='index___en' && !Object.keys(route.params).length)
+    if(!store.state.i18n.initalized)
+      redirect(`/fr/${conferenceCode}`)
+
 }
--- pages/_conferenceCode/index.vue
+++ pages/_conferenceCode/index.vue
@@ -1,41 +1,74 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-xs-6">
-         {{$t('home')}}<br>sffgsdfgsdfgs
-         {{$i18n.path({name:'calendar'})}} <br><br><br><br><br>fdadfafasadsadsf
-
-
-         <svg class="icon-clock-o"><use xlink:href="#icon-clock-o"></use></svg>
-
-      </div>
-    </div>
+  <div class='container-fluid home'>
+    <img class='hero' v-if='getHeroImage(conference)' :src='getHeroImage(conference)' :alt='`${conference.title[$i18n.locale]} logo`'/>
+    <span v-html="this.$options.filters.lstring(article.content)"></span>
   </div>
 </template>
 
 <script>
   export default {
     components: {},
-    async asyncData ({store,params,router}) {
-      console.log('index asyncData')
+    async asyncData ({app , store}) {
+      let article = (await loadArticle(app.$axios,store.state.conferences.selected.code))[0]
+      let conference = store.state.conferences.selected
+      return {
+        conference:conference,
+        article:article,
+        title:article.title,
+        image:conference.conference.heroImage || conference.conference.image
+      }
+    },
+    methods: {
+      getHeroImage:getHeroImage,
+      loadArticle:loadArticle,
+      getQuery:getQuery
+    },
+    created () {
+
+      let from = this.$store.state.routes.prevRoute
+      let locale = this.$store.state.i18n.locale
+
+      if(from.fullPath === '/' && this.$route.name === `conferenceCode___${locale}` && !this.$store.state.routes.initalized){
+        this.$store.commit('routes/I18N_INIT', true)
+        let pathName = this.$route.name.replace(`___${this.$store.state.i18n.locale}`,`___${this.$store.state.i18n.prevLocale}`)
+        this.$router.replace({ name:pathName , params: this.$route.params })
+      }
     }
+  }
+  function getHeroImage(){
+    let conference = this.$store.state.conferences.selected
+    if(!conference.conference || !conference.conference.heroImage) return false
+    return conference.conference.heroImage
+  }
+
+  function loadArticle($axios,code){
+    return $axios.$get('/api/v2017/articles', { params: getQuery(code)})
+  }
+
+  function getQuery(code){
+    let ag   = []
+    let tags = []
+
+    tags[0] = encodeURIComponent('cbd-events')
+    tags[1] = encodeURIComponent(code)
+
+    let match = { 'adminTags.title.en' : { $all: tags}}
+
+    ag.push({'$match'   : match })
+    ag.push({'$project' : { title:1, summary:1, content:1, coverImage:1}})
+    ag.push({'$sort'    : { 'meta.updatedOn':-1}})
+    ag.push({'$limit'   : 1 })
+
+    return {ag:JSON.stringify(ag)}
   }
 </script>
 
-<style>
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+<style scoped>
+.home{
+  padding-bottom: 3em;
 }
-
-.links {
-  padding-top: 15px;
-}
-.right{
-  border-left: 1px solid black;
-}
+  .hero  {
+    align-self: start;
+    width:95vw;
+  }
 </style>
