89dc2b510ba1337fe676a0a20cbb8ee2485e5d99
Randy Houlahan <randy.houlahan@cbd.int>
2018-10-24 08:11:37 -0400

    icons & splash updates

---
+++ static/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class='container-fluid home'>
+    <h3>UN Biodiversity Events</h3>
+  </div>
+</template>
+
+<script>
+
+  export default {
+
+    name:"startUrl"
+  }
+  
+</script>
+
+
