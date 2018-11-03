1b33836b6d2172c7902a0faebd7c196b3f1c8cd4
Randy Houlahan <randy.houlahan@cbd.int>
2018-11-03 11:46:47 -0400

    cleaning

--- plugins/cordova.js
+++ plugins/cordova.js
@@ -3,7 +3,6 @@ import VueCordova from 'vue-cordova';
 Vue.use(VueCordova);
 export default ({app: {router}, store}, inject) => {
     Vue.cordova.on('deviceready', () => {
-      console.log('Vue.cordova.file',cordova.file)
       Vue.cordova.file = cordova.file
       Vue.cordova.iAB = Vue.cordova.inAppBrowser = cordova.InAppBrowser
       Vue.cordova.fO2 = Vue.cordova.fileOpener2 = cordova.plugins.fileOpener2
