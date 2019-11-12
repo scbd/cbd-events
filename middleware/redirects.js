6f65694f1cf78574a67cb5c3f90815828fd6b7d1
Randy Houlahan <randy.houlahan@cbd.int>
2019-11-12 10:06:22 -0500

    remove try to detect offline

--- middleware/redirects.js
+++ middleware/redirects.js
@@ -1,18 +1,14 @@
 export default async function ({ route, redirect, store }){
-  try{
-    const { name } = route
-    const hasRouteParams = Object.keys(route.params).length
-    const isConferenceLoaded = store.state.conferences.selected
 
-    if(name.includes('offline')) return
+  const hasRouteParams = Object.keys(route.params).length
+  const isConferenceLoaded = store.state.conferences.selected
 
-    if(!isConferenceLoaded)
-      await store.dispatch('conferences/get')
+  if(!isConferenceLoaded)
+    await store.dispatch('conferences/get')
 
-    const { code } = store.state.conferences.selected
-    const isAppFistLoad = !hasRouteParams && code
+  const { code } = store.state.conferences.selected
+  const isAppFistLoad = !hasRouteParams && code
 
-    if(isAppFistLoad) redirect(`/${code}`)
-  }
-  catch(e){ redirect('/offline') }
+  if(isAppFistLoad) redirect(`/${code}`)
+
 }
