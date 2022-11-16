91c1f1e879562919b942f65f760ec2f4f6421705
Randy Houlahan <randy.houlahan@cbd.int>
2022-11-16 12:21:03 -0500

    DEV-189 fix redirect bug

--- middleware/redirects.js
+++ middleware/redirects.js
@@ -2,8 +2,8 @@ export default async function ({ route, redirect, store }){
   const hasRouteParams = Object.keys(route.params).length
   const isConferenceLoaded = store.state.conferences.selected
 
-  if(!isConferenceLoaded)
-    await store.dispatch('conferences/get')
+  if(!isConferenceLoaded) await store.dispatch('conferences/get')
+  else store.dispatch('conferences/get')
 
   const { code } = store.state.conferences.selected
   const isAppFistLoad = !hasRouteParams && code
